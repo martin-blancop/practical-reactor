dd5fd1a85eae53173daef5a619ca14ca62c2d004
martin-blancop <martibp1@gmail.com>
2024-04-17 14:43:27 +0200

    capitulo 11 acabado

--- exercises/src/test/java/c11_Batching.java
+++ exercises/src/test/java/c11_Batching.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.awt.*;
 import java.time.Duration;
 
 /**
@@ -29,9 +30,9 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void batch_writer() {
         //todo do your changes here
-        Flux<Void> dataStream = null;
-        dataStream();
-        writeToDisk(null);
+        Flux<Void> dataStream = dataStream()
+                .buffer(10)
+                .flatMap(this::writeToDisk);
 
         //do not change the code below
         StepVerifier.create(dataStream)
@@ -50,10 +51,12 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void command_gateway() {
         //todo: implement your changes here
-        Flux<Void> processCommands = null;
-        inputCommandStream();
-        sendCommand(null);
-
+        Flux<Void> processCommands = inputCommandStream()
+                .groupBy(Command::getAggregateId)
+                .concatMap(groupedCommands ->
+                        groupedCommands
+                                .flatMapSequential(this::sendCommand)
+                );
         //do not change the code below
         Duration duration = StepVerifier.create(processCommands)
                 .verifyComplete();
@@ -69,6 +72,8 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void sum_over_time() {
         Flux<Long> metrics = metrics()
+                .window(Duration.ofSeconds(1))
+                .concatMap(window -> window.reduce(0L, Long::sum))
                 //todo: implement your changes here
                 .take(10);
 
