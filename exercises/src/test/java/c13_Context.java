080aeee2c04d6b25b2804f31f98e5d6edcb0c571
martin-blancop <martibp1@gmail.com>
2024-04-18 12:20:17 +0200

    capitulo 13 terminado

--- exercises/src/test/java/c13_Context.java
+++ exercises/src/test/java/c13_Context.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 import reactor.test.StepVerifier;
 import reactor.util.context.Context;
 
@@ -32,7 +33,10 @@ public class c13_Context extends ContextBase {
      */
     public Mono<Message> messageHandler(String payload) {
         //todo: do your changes withing this method
-        return Mono.just(new Message("set correlation_id from context here", payload));
+        return Mono.deferContextual(ctx -> {
+            String correlationId = ctx.get(HTTP_CORRELATION_ID);
+            return Mono.just(new Message(correlationId, payload));
+        });
     }
 
     @Test
@@ -55,9 +59,7 @@ public class c13_Context extends ContextBase {
         Mono<Void> repeat = Mono.deferContextual(ctx -> {
             ctx.get(AtomicInteger.class).incrementAndGet();
             return openConnection();
-        });
-        //todo: change this line only
-        ;
+        }).contextWrite(Context.of(AtomicInteger.class, new AtomicInteger(0)));
 
         StepVerifier.create(repeat.repeat(4))
                     .thenAwait(Duration.ofSeconds(10))
@@ -79,11 +81,28 @@ public class c13_Context extends ContextBase {
         AtomicInteger pageWithError = new AtomicInteger(); //todo: set this field when error occurs
 
         //todo: start from here
-        Flux<Integer> results = getPage(0)
+        Flux<Integer> results = Mono.deferContextual(ctx ->
+                        getPage(ctx.get(AtomicInteger.class).get()))
+                .doOnEach(s -> {
+                    switch (s.getType()) {
+                        case ON_NEXT:
+                            s.getContextView().get(AtomicInteger.class).incrementAndGet();
+                            break;
+                        case ON_ERROR:
+                            pageWithError.set(s.getContextView().get(AtomicInteger.class).get());
+                            System.out.println(
+                                    "Error has occurred: " + s.getThrowable().getMessage());
+                            System.out.println("Error occurred at page: " + s.getContextView()
+                                    .get(AtomicInteger.class)
+                                    .getAndIncrement());
+                            break;
+                    }
+                })
+                .onErrorResume(error -> Mono.empty())
                 .flatMapMany(Page::getResult)
                 .repeat(10)
-                .doOnNext(i -> System.out.println("Received: " + i));
-
+                .doOnNext(i -> System.out.println("Received: " + i))
+                .contextWrite(Context.of(AtomicInteger.class, new AtomicInteger(0)));
 
         //don't change this code
         StepVerifier.create(results)
